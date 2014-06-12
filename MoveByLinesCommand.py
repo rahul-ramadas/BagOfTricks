924e54635d6a7c8deeb2d2351ddf09034568ecce
rahul-ramadas <rahul-ramadas@users.noreply.github.com>
2014-06-11 22:26:40 -0700

    A command to move by multiple lines

---
+++ MoveByLinesCommand.py
@@ -0,0 +1,9 @@
+import sublime
+import sublime_plugin
+
+
+class MoveByLinesCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, forward=True, number_of_lines=1):
+        for _ in range(number_of_lines):
+            self.view.run_command('move', {"by": "lines, "forward": forward})
