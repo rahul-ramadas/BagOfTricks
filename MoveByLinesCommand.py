1f6deabb48c7e905803dc276060e5d6ee690a3db
rahul-ramadas <rahul-ramadas@users.noreply.github.com>
2014-06-17 08:50:11 -0700

    Support for extend

--- MoveByLinesCommand.py
+++ MoveByLinesCommand.py
@@ -4,6 +4,6 @@ import sublime_plugin
 
 class MoveByLinesCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit, forward=True, number_of_lines=1):
+    def run(self, edit, forward=True, extend=False, number_of_lines=1):
         for _ in range(number_of_lines):
-            self.view.run_command('move', {"by": "lines", "forward": forward})
+            self.view.run_command('move', {"by": "lines", "forward": forward, "extend": extend})
